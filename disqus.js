e588f8a3fa0a10c3524aca24948d896109d8f72f
Rob Loach <robloach@gmail.com>
2011-03-29 10:53:13 -0400

    Issue #1107356: Move to Drupal.settings instead of the inline Disqus JavaScript code.

--- disqus.js
+++ disqus.js
@@ -1,24 +1,59 @@
+var disqus_shortname = '';
+var disqus_developer = 0;
+var disqus_url = '';
+var disqus_title = '';
+var disqus_identifier = '';
+var disqus_config = null;
+var disqus_def_name = null;
+var disqus_def_email = null;
 
 /**
  * Drupal Disqus behaviors.
  */
 Drupal.behaviors.disqus = function(context) {
+  // Process any Disqus links.
   if (Drupal.settings.disqusCommentDomain || false) {
-    // Create the query.
-    var query = '?';
-    jQuery("a[href$='#disqus_thread']").each(function(i) {
-      query += 'url' + i + '=' + encodeURIComponent($(this).attr('href')) + '&';
-    });
+    // Change the Disqus shortname.
+    disqus_shortname = Drupal.settings.disqusCommentDomain;
 
-    // Make sure we are actually processing some links.
-    if (query.length > 2) {
-      // Make the AJAX call to get the number of comments.
+    // Make the AJAX call to get the number of comments.
+    jQuery.ajax({
+      type: 'GET',
+      url: 'http://' + disqus_shortname + '.disqus.com/count.js',
+      dataType: 'script',
+      cache: true
+    });
+  }
+
+  // Process any Disqus threads.
+  if (Drupal.settings.disqus || false) {
+    // Only query if the Disqus thread exists.
+    if (jQuery("#disqus_thread").length) {
+      // Construct the settings.
+      var disqus = Drupal.settings.disqus;
+      disqus_shortname = disqus.shortname;
+      disqus_developer = disqus.developer || 0;
+      disqus_url = disqus.url;
+      disqus_title = disqus.title;
+      disqus_identifier = disqus.identifier;
+      disqus_def_name = disqus.name || null;
+      disqus_def_email = disqus.email || null;
+
+      // Assign the language.
+      if (disqus.language || false) {
+        disqus_config = function() {
+          this.language = disqus.language;
+        };
+      }
+
+      // Make the AJAX call for the comment thread.
       jQuery.ajax({
         type: 'GET',
-        url: 'http://disqus.com/forums/' + Drupal.settings.disqusCommentDomain + '/get_num_replies.js' + query,
+        url: 'http://' + disqus_shortname + '.disqus.com/embed.js',
         dataType: 'script',
         cache: true
       });
     }
   }
 };
+
